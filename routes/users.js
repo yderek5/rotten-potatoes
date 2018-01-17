c23068c595acc3aea69b66cbb6826e61cd56e737
yderek5 <yderek5@gmail.com>
2018-01-16 21:57:45 -0600

    add review and game routes

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users profile page */
 router.get('/:username', function(req, res) {
   res.render('./users/show', {title: 'show'});
 });
 
-router.get('/:username/:edit', function(req, res) {
+/* GET user edit page */
+router.get('/:username/edit', function(req, res) {
   res.render('./users/edit', {title: 'edit'});
 });
 
