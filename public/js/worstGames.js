8857582fc506d86cab0591e0c7c95b632b04728f
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2018-01-17 20:50:13 -0600

    working apiRoutes

--- public/js/worstGames.js
+++ public/js/worstGames.js
@@ -1,11 +1,10 @@
 // console.log("loaded");
 
-$.get("apiRoutes/api/topTenGames", function(data){
+$.get("apiRoutes/api/worstGames", function(data){
     var addTable = $("<ul>");
     for (var i = 0; i < data.length; i++){
         addTable.append("<li>" + data[i].name + "</li>"); 
         console.log(addTable); 
     };
-    $("#best-games-list").append(addTable);
-});
-
+    $("#worst-games-list").append(addTable);
+})
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,24 +9,11 @@ router.get('/api/topTenGames', function(req, res){
     })
 });
 
+router.get('/api/worstGames', function(req, res){
+    Game_table.findAll({}).then(function(results){
+        console.log(results);
+        res.json(results);
+    })
+});
+
 module.exports = router;
-
-
-
-
-// /* GET home page. */
-// router.get('/', function(req, res) {
-//   res.render('./home/index', { title: 'Home' });
-// });
-
-// /* GET specific game info */
-// router.get('/games/:gameTitle', function(req, res) {
-//   res.render('./home/game', { title: 'single game' });
-// });
-
-// /* GET list of all games we have on record */
-// router.get('/games', function(req, res) {
-//   res.render('./home/gameList', {title: 'game list'});
-// });
-
-// module.exports = router;
