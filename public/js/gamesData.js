33bf7fc80a0ceabafc7108a205893e99d75896a7
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2018-01-18 13:23:16 -0600

    formatting

--- public/js/gamesData.js
+++ public/js/gamesData.js
@@ -1,18 +1,19 @@
 //get list of top ten games
 $.get("apiRoutes/api/topTenGames", function(data){
-    var addTable = $("<ul>");
+    var addTable = $("<ul class='list-group game-list'>");
     for (var i = 0; i < data.length; i++){
-        addTable.append("<li>" + data[i].name + "</li>"); 
+        addTable.append("<li class='list-group-item game'><span class='badge'>+3.5</span>" + data[i].name + "</li>"); 
         console.log(addTable); 
     };
+    addTable.append("</ul>")
     $("#best-games-list").append(addTable);
 });
 
 //get list of worst games
 $.get("apiRoutes/api/worstGames", function(data){
-    var addTable = $("<ul>");
+    var addTable = $("<ul class='list-group game-list'>");
     for (var i = 0; i < data.length; i++){
-        addTable.append("<li>" + data[i].name + "</li>"); 
+        addTable.append("<li class='list-group-item game'><span class='badge'>+3.5</span>" + data[i].name + "</li>"); 
         console.log(addTable); 
     };
     $("#worst-games-list").append(addTable);
