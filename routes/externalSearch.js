e486ad0c7b7392c8f0c487839e04769592bba35a
Leo Galey IV <leog4@KCSF.local>
2018-01-26 17:21:24 -0600

    add functionality to search for games via external api

--- routes/externalSearch.js
+++ routes/externalSearch.js
@@ -1,49 +1,59 @@
 var request = require('request');
-module.exports = {
-    getGames: function(req,res){
+module.exports =  function(req,res){
+
 
         //console.log('inside route');
     
         var url = 'https://www.giantbomb.com/api/games/?api_key=2e5e0ace4e2ef2fc79d258dd9156d7fbda0656c7&format=json';
                 url += '&field_list=deck,image,id,guid,name,original_release_date,platforms,original_game_rating';
                 url += '&limit=10';
-                //url += '&filter=name:' + req.query.externalSearchTerm;
+                url += '&filter=name:' + req.query.externalSearchTerm;
                 //url += '&filter=name:' + req.params.filter;
         
         var encoded_url = encodeURI(url);
     
-        //console.log(encoded_url);
+        console.log(encoded_url);
     
         request(encoded_url , {headers: { 'User-Agent': 'RottenPotatoes' }},function (error, response, body) {
             if(error){
                 console.log('Error: ' + error);
-                response.send('An error has occured, please try again');
+                res.send('An error has occured, please try again');
             }
     
             var data = JSON.parse(body);
             var resultCount = data.results.length;
             var games = [];
+
+            console.log(resultCount);
             if(resultCount > 0){
     
                 for(i=0;i<resultCount;i++){
                     var game = data.results[i];
-    
+                    
+
+
+
                     var gameObject = {
                         description:  game.deck,
                         image_thumbnail: game.image.thumb_url, //string
-                        image: game.image.screen_url, //string
+                        image_original: game.image.original_url, //string
                         external_id: game.id,
                         guid: game.guid, 
                         name:  game.name, //string
-                        original_release_date: game.original_release_date,
-                        platforms: []
+                        original_release_date: game.original_release_date
                         }
-    
-                    for (y=0;y<game.platforms.length;y++){
-                        gameObject.platforms.push(game.platforms[y].name);
-                    }
-    
-                    games.push(gameObject);
+
+                    var push = true;
+
+                    for (var key in gameObject){
+                        if(!gameObject[key]){
+                            push = false;
+                        }
+                    };
+
+                    if (push){
+                        games.push(gameObject);
+                    };
                 }
     
        
@@ -53,10 +63,10 @@ module.exports = {
                 res.send('No results found, please try another search');
             }
             res.render('./home/externalSearch',{games:games});
+            //res.send(games);
         })
     
     
     
         //res.render('./home/results.hbs', {searchString: req.params.filter});
     }
-}
