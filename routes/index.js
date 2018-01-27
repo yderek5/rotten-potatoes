2b1170f5d221d9164b82e04292229c36e83056ff
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2018-01-26 18:38:19 -0600

    redirects to the game page after adding the game

--- routes/index.js
+++ routes/index.js
@@ -188,6 +188,10 @@ module.exports = function(app,passport){
           image_original: req.body.game.image_original,
           external_id: req.body.game.external_id,
           original_release_date: req.body.game.original_release_date,
+        }).then(function(result){
+          console.log(result);
+          var url = "/games/" + result.dataValues.id;
+          res.send({url:url})
         })
       }
     })
